1e7ab68b3098c5fa2f05b9ec0cbc3a4f8c2c717a
ZhBauyrzhan <zhnbauyrzhan@gmail.com>
2022-11-25 20:54:40 +0600

    Changed task 8

--- lab2/problem8.py
+++ lab2/problem8.py
@@ -14,9 +14,9 @@ def substract_from_bank_account(sum: float):
 def money_conversion(sum: float, init, wanted):
     match init, wanted:
         case 'USD' as init, 'KZT' as wanted:
-            return current_sum * 470
+            return sum * 470
         case 'KZT' as init, 'USD' as wanted:
-            return current_sum / 470
+            return sum / 470
         case _:
             return "We don't do this"
 
@@ -27,4 +27,4 @@ print(current_sum)
 substract_from_bank_account(200)
 print(current_sum)
 
-print( money_conversion(300, 'KZT', 'USD') )
+print(money_conversion(300, 'KZT', 'USD'))
